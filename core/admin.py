3be60f3b3d694382e3a5c9af2c2410c4877b955c
Gustavo Carvalho <gt.salles@gmail.com>
2015-10-31 19:07:28 -0300

    Refatorando models para permitir que um participante tenha mais de um certificado

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import Participante
+from .models import Certificado, Participante
 
+admin.site.register(Certificado)
 admin.site.register(Participante)
--- core/forms.py
+++ core/forms.py
@@ -1,4 +1,6 @@
 # coding: utf-8
+import os
+import zipfile
 
 from django import forms
 from django.http import HttpResponse
@@ -6,21 +8,28 @@ from .models import Participante
 
 
 class CertificadoForm(forms.Form):
-    cpf = forms.RegexField(required=True, regex=r'\A\d{3}\.\d{3}\.\d{3}-\d{2}', error_message=u'Digite um CPF válido')
-    cpf = forms.CharField(label="Número do CPF:")
-    cpf.widget.attrs.update({'autofocus':'autofocus', 'required':'required'})
+    cpf = forms.RegexField(label=u'Número do CPF:', required=True,
+                           regex=r'\A\d{3}\.\d{3}\.\d{3}-\d{2}',
+                           error_message=u'Digite um CPF válido')
+    cpf.widget.attrs.update({'autofocus': 'autofocus', 'required': 'required'})
 
     error_css_class = 'alert alert-danger'
 
     def process(self):
         cpf = self.cleaned_data['cpf']
         participante = Participante.objects.get(cpf=cpf)
-        return HttpResponse(participante.certificado, content_type='application/pdf')
+        if participante.certificado_set.count() == 1:
+            return HttpResponse(participante.certificado_set.latest('id').arquivo,
+                                content_type='application/pdf')
+        # TODO: Implementar quando o participante tiver mais de um certificado
 
     def clean_cpf(self):
         cpf = self.cleaned_data['cpf']
         cpf = cpf.replace('.', '').replace('-', '')
-        participante = Participante.objects.filter(cpf=cpf)
-        if not participante.exists():
+        try:
+            participante = Participante.objects.get(cpf=cpf)
+        except Participante.DoesNotExist:
             raise forms.ValidationError(u'CPF não encontrado')
+        if not participante.certificado_set.exists():
+            raise forms.ValidationError(u'Participante sem certificados cadastrados')
         return cpf
